d95b9ea3b64d74973a08567a88968ed0f3e3ae91
Ben Agricola <ben+git@agrico.la>
2024-11-01 23:21:56 +0000

    Add spindle type, std and fwdrev control schemes
    Spindles now have a type, which defaults to standard (`T0`).
    In standard mode, spindles have an enable and a direction port:
    
      Forward: enable=1, direction=0
      Reverse: enable=1, direction=1
    
    Setting fwdrev (`T1`)  mode on the spindle changes the ports
    to represent forward and reverse:
    
      Forward: forward=1, reverse=0
      Reverse: forward=0, reverse=1
    
    Signed-off-by: Ben Agricola <ben+git@agrico.la>

--- src/Platform/Platform.cpp
+++ src/Platform/Platform.cpp
@@ -3986,8 +3986,8 @@ GCodeResult Platform::ConfigurePort(GCodeBuffer& gb, const StringRef& reply) THR
 		}
 	case 32:	// R
 		{
-			const uint32_t slot = gb.GetLimitedUIValue('R', MaxSpindles);
-			return spindles[slot].Configure(gb, reply);
+			const uint32_t spindleNumber = gb.GetLimitedUIValue('R', MaxSpindles);
+			return spindles[spindleNumber].Configure(spindleNumber, gb, reply);
 		}
 
 #if SUPPORT_LED_STRIPS
